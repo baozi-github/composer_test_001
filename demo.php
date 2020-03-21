e348c35a9a7727825939570b6cd9a91ea86e3306
baozi <894336866.com>
2020-03-21 10:28:28 +0800

    我的第一个测试版本

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once './vendor/autoload.php';
+use Composer_test\Composer_test\Tools;
+Tools::Hello();
---
+++ src/Composer_test/Composer_test/Tools.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Composer_test\Composer_test;
+
+class Tools
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function Hello()
+    {
+        echo "hello baozi";
+    }
+}
